1a7f9532619add381b284aa00d0ebfb0d2a97e6a
mosaab <musab.ehab@gmail.com>
2018-08-12 13:26:54 +0200

    add simple searching functionality

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,16 @@ Route::get('/tag/{id}',[
     'as'=>'tag.single'
 ]);
 
+Route::get('/results',function(){
+    $posts = \App\Post::where('title','like','%' . request('query') . '%')->get();
+
+    return view('results')->with('posts',$posts)
+                            ->with('title','Search results: '.request('query'))
+                            ->with('categories',\App\Category::take(5)->get())
+                            ->with('settings',\App\Setting::first())
+                            ->with('query',request('query'));
+});
+
 Auth::routes();
 
 
